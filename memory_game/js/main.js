d7a1007bdabefd83e528efba30e293b87c902441
Abdulrhman Emad Khoja <a.khouja1994@gmail.com>
2019-04-12 17:39:56 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,22 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king"; 
-console.log("User flipped " + cardThree);
+alert('Hello, friends.');
+var cards = ["queen","king","queen","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+var cardThree = cards[2];
+var cardFour = cards[3];
+cardsInPlay.push(cardOne);
+console.log("Up and running!"); 
+console.log("User flipped " + cardsInPlay[0]);
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+if (cardsInPlay[0]===cardsInPlay[1])
+{
+	alert("You found a match!");
+}
+else
+{
+	cardsInPlay=[]
+	alert("Sorry, try again");
+}
+console.log(cardsInPlay[0]);
