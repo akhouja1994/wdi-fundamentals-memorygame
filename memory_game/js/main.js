a419a08e56dde5f08328eebed664c92639e15280
Abdulrhman Emad Khoja <a.khouja1994@gmail.com>
2019-04-14 16:13:46 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen","king","queen","king"];
+var cards = [
+	{
+	rank:'queen',
+	suit: 'hearts',
+	cardImage:'src/images/images/queen-of-hearts.png'
+	},
+	{
+	rank:'king',
+	suit:'hearts',
+	cardImage:'src/images/images/king-of-hearts.png'
+	},
+	{
+	rank:'queen',
+	suit:'diamonds',
+	cardImage:'src/images/images/queen-of-diamonds.png'
+	},
+	{
+	rank:'king',
+	suit:'diamonds',
+	cardImage:'src/images/images/king-of-diamonds.png'
+	}
+	];
 var cardsInPlay = [];
 var checkForMatch =function(memorey)
 {
@@ -14,8 +35,9 @@ var checkForMatch =function(memorey)
 }
 var flipCard = function (cardId)
 	{
-	cardsInPlay.push(cards[cardId]);
-	console.log("user flipped" + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("user flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
 	}
 flipCard(0);
 flipCard(1);
