4e37de7df31ee4733184f7a3323d30b8a808f006
Abdulrhman Emad Khoja <a.khouja1994@gmail.com>
2019-04-16 18:31:51 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,24 +21,43 @@ var cards = [
 	}
 	];
 var cardsInPlay = [];
+
+var createBoard=function()
+{
+	for (var i = 0; i < cards.length; i++) {
+    // Logic here
+
+    var cardElement = document.createElement('img');
+    var gameBoard = document.getElementById('game-board');
+    cardElement.setAttribute('src','src/images/images/back.png');
+    cardElement.setAttribute('data-id',i);
+    gameBoard.appendChild(cardElement);
+    cardElement.addEventListener('click',function(e){flipCard(this);
+    	if (cardsInPlay.length >=2){checkForMatch(cardsInPlay);}});
+}
+	
+}
+var flipCard = function (element)
+	{
+	var cardId = element.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	element.setAttribute('src',cards[cardId].cardImage);
+	}
+
 var checkForMatch =function(memorey)
 {
 		if (memorey[0]===memorey[1])
 		{
-			console.log("You found a match!");
+			alert("You found a match!");
+			
 		}
 
 	else
 		{
-			console.log("Sorry, try again");
+			alert("Sorry, try again");
+
 		}
+		cardsInPlay = [];
 }
-var flipCard = function (cardId)
-	{
-	cardsInPlay.push(cards[cardId].rank);
-	console.log("user flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	}
-flipCard(0);
-flipCard(1);
-checkForMatch(cardsInPlay);
+
+createBoard();
